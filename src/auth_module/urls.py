22c8adbabec76de18117006e9d1264916a2f7ab4
ali <ali.darzi.1354@gmail.com>
2024-11-09 04:05:14 +0330

    LogEmailView just added to url !

--- src/auth_module/urls.py
+++ src/auth_module/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("available/", views.AvailableView.as_view(), name="available"),
     path("send-email-otp/", views.EmailSendOTPView.as_view(), name="send_email_otp"),
     path("check-email-otp/", views.check_email_otp, name="check_email_otp"),
+    path("log-user/", views.LogEmailView.as_view(), name="log_user"),
 
 ]
